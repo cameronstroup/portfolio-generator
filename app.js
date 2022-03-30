219e56067d90025c8edc4d3af0405af3efe51c83
cameronstroup <90347622+cameronstroup@users.noreply.github.com>
2022-03-30 10:51:55 -0500

    lesson 1

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Hello Node!");
+const printProfileData = (profileDataArr) => {
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
+
+  console.log("================");
+
+  // Is the same as this...
+  profileDataArr.forEach((profileItem) => {
+    console.log(profileItem);
+  });
+};
