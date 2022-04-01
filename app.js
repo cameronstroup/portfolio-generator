1915696439b85a888085b49b1dfd00f8f1f76924
cameronstroup <90347622+cameronstroup@users.noreply.github.com>
2022-04-01 18:10:06 -0500

    next step

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-const printProfileData = (profileDataArr) => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+// const fs = require("fs");
+// const generatePage = require("./src/page-template");
 
-  console.log("================");
+// const profileDataArgs = process.argv.slice(2);
 
-  // Is the same as this...
-  profileDataArr.forEach((profileItem) => {
-    console.log(profileItem);
-  });
-};
+// console.log(profileDataArgs);
+
+// const [name, github] = profileDataArgs;
+
+// console.log(name, github);
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile("./index.html", pageHTML, (err) => {
+//   if (err) throw err;
+
+//   console.log("Portfolio complete! Check out index.html to see the output!");
+// });
+const profileDataArgs = process.argv;
+console.log(profileDataArgs);
